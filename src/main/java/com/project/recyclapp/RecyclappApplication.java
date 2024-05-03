529dac3077214d4ba3939421a46fd356b2880091
Luis_Ospina_Pava <luisospina@wposs.com>
2024-05-03 14:31:28 -0500

    SPRING INIT
    Se inicializa el proyecto de Spring

---
+++ src/main/java/com/project/recyclapp/RecyclappApplication.java
@@ -0,0 +1,13 @@
+package com.project.recyclapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecyclappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecyclappApplication.class, args);
+	}
+
+}
